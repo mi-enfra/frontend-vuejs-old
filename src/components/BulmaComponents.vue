96c19cb35d859e4be127e94d7545cb32b3f1a954
Marvin Isaac <misaac@pushnami.com>
2019-08-20 15:38:02 -0400

    :wrench: Setup of sample Vue.js app initiated with CLI
    Also included is a sample component and integration with Bulma
    CSS framework.

---
+++ src/components/BulmaComponents.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="columns is-centered">
+        <div class="column is-narrow">
+            <input class="input">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'BulmaComponents'
+}
+</script>
